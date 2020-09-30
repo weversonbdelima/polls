25e92269c803157ea3213aa6da9afe6814aeda38
weversonbdelima <weversonbdelima@gmail.com>
2020-09-30 20:55:29 -0300

    Deploying the app

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'xapu4moil#i7)%%cfub-^5i*at7r$@#w_t95p_8io-cufw5*c*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
+ALLOWED_HOSTS = []
 
 
 # Application definition
